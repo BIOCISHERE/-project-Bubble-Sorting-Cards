b0d5974c217e28b5e748ee59fee6d9e9dd90eb24
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-14 02:33:17 +0000

    avances, arreglar el sortCardObj

--- src/app.js
+++ src/app.js
@@ -35,15 +35,21 @@ function randomSuit() {
   return suit[indesSuit];
 }
 
+let cardObj = []; // array where cards value  are pushed
+
 let drawbtn = document.querySelector(".drawButton");
 let sortbtn = document.querySelector(".sortButton");
 let amountCard = document.querySelector(".amountCard");
 let randomCards = document.querySelector(".randomCards");
+let sortCards = document.querySelector(".bubbleSortLog");
 
+// draw button function start
 drawbtn.addEventListener("click", () => {
   let randomCards = document.querySelector(".randomCards");
   let numberOfCards = `${amountCard.value}`;
   let olCard = document.querySelector(".card");
+
+  cardObj.length = 0;
 
   if (olCard == null) {
     console.log("dosn't exist");
@@ -60,14 +66,19 @@ drawbtn.addEventListener("click", () => {
     div.classList.add("card");
     div.classList.add(randomSuit());
     div.innerHTML = randomNumber();
+
     randomCards.appendChild(div);
+    cardObj.push({ value: div.innerHTML, class: div.className.split(" ") });
   }
+
+  console.log(cardObj);
 });
+// draw funtion end
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+      if (arr[j].value > arr[j + 1].value) {
         let aux = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = aux;
@@ -76,3 +87,58 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+function switchToNumber(arr) {
+  switch (arr.value) {
+    case "A":
+      arr.value = 1;
+    case "j":
+      arr.value = 11;
+    case "Q":
+      arr.value = 12;
+    case "K":
+      arr.values = 13;
+  }
+}
+
+let sortCardObj = [];
+
+sortbtn.addEventListener("click", () => {
+  sortCardObj.length = 0;
+  for (let i = 0; i < cardObj.length; i++) {
+    sortCardObj.push(cardObj[i]);
+  }
+
+  let objIndexA = sortCardObj.findIndex(obj => obj.value == "A");
+  let objIndexJ = sortCardObj.findIndex(obj => obj.value == "J");
+  let objIndexQ = sortCardObj.findIndex(obj => obj.value == "Q");
+  let objIndexK = sortCardObj.findIndex(obj => obj.value == "K");
+  console.log(sortCardObj[objIndexA]);
+  console.log(sortCardObj[objIndexJ]);
+  console.log(sortCardObj[objIndexQ]);
+  console.log(sortCardObj[objIndexK]);
+
+  for (let j = 0; j < 1; j++) {
+    if (sortCardObj[objIndexA]) {
+      sortCardObj[objIndexA] = "1";
+      console.log("changed A value to 1");
+    }
+    if (sortCardObj[objIndexJ]) {
+      sortCardObj[objIndexJ] = "11";
+      console.log("changed J value to 11");
+    }
+    if (sortCardObj[objIndexQ]) {
+      sortCardObj[objIndexQ] = "12";
+      console.log("changed Q value to 12");
+    }
+    if (sortCardObj[objIndexK]) {
+      sortCardObj[objIndexK] = "13";
+      console.log("changed K value to 13");
+    }
+  }
+  console.log(sortCardObj);
+  //bubbleSort(cardObj);
+});
+//let ox = document.createElement("div");
+//ox.classList.add("card", "club");
+//ox.innerHTML = "6";
+//sortCards.appendChild(ox);
